2030ffb1873880c8208ff791cc19ef8e61454517
josegoval <josegovalmusic@gmail.com>
2022-01-22 18:07:09 +0100

    WIP

--- Source/BlueprintsToCpp/Quests/QuestManager.cpp
+++ Source/BlueprintsToCpp/Quests/QuestManager.cpp
@@ -6,22 +6,36 @@
 // Sets default values
 AQuestManager::AQuestManager()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
 }
 
 // Called when the game starts or when spawned
 void AQuestManager::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
 
 // Called every frame
 void AQuestManager::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 
+int32 AQuestManager::CompleteQuest_Implementation(FName QuestId, bool CompleteWholeQuest)
+{
+	const int32 QuestIndex = GetQuestIndex(QuestId);
+	UE_LOG(LogTemp, Warning, TEXT("GOING IN %d", QuestIndex))
+	if (QuestIndex == -1) return QuestIndex;
+
+	FQuestInfo Quest = QuestList[QuestIndex];
+	if (CompleteWholeQuest)
+	{
+		Quest.Progress = Quest.ProgressTotal;
+	}
+	else
+	{
+		Quest.Progress = FMath::Min(Quest.Progress + 1, Quest.ProgressTotal);
+	}
+	return QuestIndex;
+}
--- Source/BlueprintsToCpp/Quests/QuestManager.h
+++ Source/BlueprintsToCpp/Quests/QuestManager.h
@@ -11,18 +11,29 @@ UCLASS()
 class BLUEPRINTSTOCPP_API AQuestManager : public AActor
 {
 	GENERATED_BODY()
-	
-public:	
+
+public:
 	// Sets default values for this actor's properties
 	AQuestManager();
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
-public:	
+	UFUNCTION(BlueprintPure, BlueprintImplementableEvent)
+	int32 GetQuestIndex(FName QuestId);
+public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+	
+	/**
+	 * @brief Completes the given quest if possible. It returns -1 if the
+	 * `FQuestInfo` with QuestId wasn't found.
+	 * @param QuestId 
+	 * @param CompleteWholeQuest 
+	 * @return 
+	 */
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	int32 CompleteQuest(FName QuestId, bool CompleteWholeQuest);
 protected:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	TArray<FQuestInfo> QuestList;
