09cc607ad97c5195f3fc4416054df41916a50aec
josegoval <josegovalmusic@gmail.com>
2022-01-22 16:54:43 +0100

    feat: initial commit (up to UStructs)

---
+++ Source/BlueprintsToCpp/Quests/QuestInfo.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "QuestInfo.generated.h"
+
+/**
+ * 
+ */
+USTRUCT(BlueprintType)
+struct BLUEPRINTSTOCPP_API FQuestInfo
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FString Name;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FName QuestId;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 Progress;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 ProgressTotal;
+};
